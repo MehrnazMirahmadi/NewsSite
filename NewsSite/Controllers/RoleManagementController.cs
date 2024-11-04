d543c9b6e276e8828dab413a30f6fd52199d13c7
mehrnazmirahmadi <mehrnazmirahmadi2@gmail.com>
2024-11-04 14:16:30 +0330

    AddUserManegment

--- NewsSite/Controllers/RoleManagementController.cs
+++ NewsSite/Controllers/RoleManagementController.cs
@@ -24,6 +24,7 @@ namespace NewSite.Controllers
         {
             return View();
         }
+        #region AddRole
         [HttpPost]
         public async Task<IActionResult> AddNewRole(RoleAddEditModel roleAddEditModel)
         {
@@ -49,5 +50,84 @@ namespace NewSite.Controllers
             }
 
         }
+        #endregion
+        #region GET: RoleManagement/Edit/{id}
+        public async Task<IActionResult> Edit(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var model = new RoleAddEditModel
+            {
+                RoleID = role.Id,
+                RoleName = role.Name,
+                Description = role.Description
+            };
+
+            return View(model);
+        }
+
+        #endregion
+        #region Edit
+        // POST: RoleManagement/Edit/{id}
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleAddEditModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var role = await roleManager.FindByIdAsync(model.RoleID.ToString());
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            role.Name = model.RoleName;
+            role.Description = model.Description;
+
+            var updateResult = await roleManager.UpdateAsync(role);
+            if (updateResult.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in updateResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View(model);
+            }
+        }
+        #endregion
+        #region Delete
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+            if (role == null)
+            {
+                return NotFound();
+            }
+
+            var deleteResult = await roleManager.DeleteAsync(role);
+            if (deleteResult.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var error in deleteResult.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("Delete", new RoleAddEditModel { RoleID = role.Id, RoleName = role.Name, Description = role.Description });
+            }
+        }
+        #endregion
     }
 }
